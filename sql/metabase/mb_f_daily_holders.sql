89c6fa22fd720be248bc9f8d67901c2eb398fa42
Tim Sugaipov <tim@Tims-MacBook-Pro.local>
2021-08-18 20:44:15 +0100

    clean up

--- sql/metabase/mb_f_daily_holders.sql
+++
@@ -1,8 +0,0 @@
-{%- from 'sql/macros/dynamic_source.sql' import dynamic_src  -%}
-
-SELECT dt
-     , token_address
-     , n_holders
-     , total_balance
-
-FROM {{ dynamic_src("models.f_daily_holders") }}
--- sql/metabase/mb_top_100_tokens.sql
+++
@@ -1,6 +0,0 @@
-{%- from 'sql/macros/dynamic_source.sql' import dynamic_src  -%}
-
-SELECT address
-     , ranking AS etherscan_ranking
-
-FROM {{ dynamic_src("models.top_100_tokens") }}
